17048ae17a8343749ced8f6979172fe9b92530e7
Andrew Platonov <andrew.platonov@protonmail.com>
2023-05-26 17:46:15 +0200

    validate_http_request stub added

--- app/src/jsight.rs
+++ app/src/jsight.rs
@@ -2,8 +2,19 @@ use std::ffi::{CStr};
 use std::str;
 use libloading::{Symbol, Library};
 use libc::c_char;
-
 use once_cell::sync::OnceCell;
+
+pub struct ValidationError {
+    pub reported_by: String,
+    pub r#type     : String,
+    pub code       : String,
+    pub title      : String,
+    pub detail     : String,
+    // pub Position: *mut ErrorPosition,
+    // pub Trace: *mut *mut ::std::os::raw::c_char,
+}
+
+
 
 static JSIGHT_STAT_SYMBOL_CELL: OnceCell<Symbol<unsafe extern fn() -> *const c_char>> = OnceCell::new();
 static LIB_CELL: OnceCell<Library> = OnceCell::new();
@@ -26,3 +37,52 @@ pub fn stat() -> Result<&'static str, Box<dyn std::error::Error>> {
         Ok(rust_str)
     }
 }
+
+pub fn validate_http_request(
+    api_spec_path  : &str, 
+    method         : &str, 
+    uri            : &str, 
+    request_headers: i32, 
+    request_body   : &[u8]
+) -> Result<(), ValidationError> {
+    Ok(())
+}
+
+/*
+pub fn JSightValidateHttpRequest(
+    apiSpecFilePath: *mut ::std::os::raw::c_char,
+    requestMethod: *mut ::std::os::raw::c_char,
+    requestURI: *mut ::std::os::raw::c_char,
+    requestHeaders: *mut *mut Header,
+    requestBody: *mut ::std::os::raw::c_char,
+) -> *mut ValidationError;
+
+#[repr(C)]
+#[derive(Debug, Copy, Clone)]
+pub struct ValidationError {
+    pub ReportedBy: *mut ::std::os::raw::c_char,
+    pub Type: *mut ::std::os::raw::c_char,
+    pub Code: ::std::os::raw::c_int,
+    pub Title: *mut ::std::os::raw::c_char,
+    pub Detail: *mut ::std::os::raw::c_char,
+    pub Position: *mut ErrorPosition,
+    pub Trace: *mut *mut ::std::os::raw::c_char,
+}
+
+#[repr(C)]
+#[derive(Debug, Copy, Clone)]
+pub struct ErrorPosition {
+    pub Filepath: *mut ::std::os::raw::c_char,
+    pub Index: *mut ::std::os::raw::c_int,
+    pub Line: *mut ::std::os::raw::c_int,
+    pub Col: *mut ::std::os::raw::c_int,
+}
+
+#[repr(C)]
+#[derive(Debug, Copy, Clone)]
+pub struct Header {
+    pub Name: *mut ::std::os::raw::c_char,
+    pub Value: *mut ::std::os::raw::c_char,
+}
+
+*/
--- app/src/main.rs
+++ app/src/main.rs
@@ -2,16 +2,30 @@ mod jsight;
 
 use std::convert::Infallible;
 use std::net::SocketAddr;
-
-use http_body_util::Full;
-use hyper::body::Bytes;
+use chrono::Local;
+use http_body_util::{Full, BodyExt};
+use hyper::body::{Bytes, Incoming};
 use hyper::server::conn::http1;
 use hyper::service::service_fn;
-use hyper::{Request, Response};
+use hyper::{Request, Response, Method};
 use tokio::net::TcpListener;
 
-async fn handle_request(_: Request<hyper::body::Incoming>) -> Result<Response<Full<Bytes>>, Infallible> {
-    println!("HTTP request");
+async fn handle_request(req: Request<Incoming>) -> Result<Response<Full<Bytes>>, Infallible> {
+
+    let api_spec_path = "/opt/app/src/my-api-spec.jst";
+    let method = get_current_method(&req);
+    let uri = req.uri().to_string();
+    let request_body = req.collect().await.unwrap().to_bytes();
+
+    jsight::validate_http_request(
+        api_spec_path,
+        &method,
+        &uri,
+        123,
+        &request_body
+    );
+
+    println!("{} {} {}", Local::now().format("%Y-%m-%d %H:%M:%S"), method, uri);
     Ok(Response::new(Full::new(Bytes::from("Hello, World!"))))
 }
 
@@ -26,6 +40,7 @@ async fn main() -> Result<(), Box<dyn std::error::Error + Send + Sync>> {
     // initialize server
     let addr = SocketAddr::from(([0, 0, 0, 0], 8000));
     let listener = TcpListener::bind(addr).await?;
+    println!("Listening on http://{}", addr);
 
     loop {
         let (stream, _) = listener.accept().await?;
@@ -37,5 +52,16 @@ async fn main() -> Result<(), Box<dyn std::error::Error + Send + Sync>> {
                 println!("Error serving connection: {:?}", err);
             }
         });
+    }
+}
+
+fn get_current_method(req: &Request<Incoming>) -> String {
+    match req.method() {
+        &Method::GET    => "GET".to_string(),
+        &Method::POST   => "POST".to_string(),
+        &Method::PUT    => "PUT".to_string(),
+        &Method::DELETE => "DELETE".to_string(),
+        // Add more HTTP methods as needed
+        _ => "UNKNOWN".to_string(),
     }
 }
